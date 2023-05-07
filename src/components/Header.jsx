103c4d0676d59bed48c306959b9f5b22736d1d56
Sushil Adhikari <sushiladh2059@gmail.com>
2023-05-07 15:36:32 +0545

    2nd try sarika

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-const Header = ({ name, username, timestamp, avatar }) => {
+const Header = ({ name, username }) => {
   return (
     <div className="tweet-header">
       <h3 className="username">{name}</h3>
       <span className="handle">{username}</span>
-      <span className="timestamp">{timestamp}</span>
+      <span className="timestamp">- 5h</span>
     </div>
   );
 };
--- src/components/TweetBody.jsx
+++ src/components/TweetBody.jsx
@@ -1,14 +1,15 @@
-const TweetBody = ({ imagesrc, content }) => {
+import { useNavigate } from "react-router-dom";
+
+const TweetBody = ({ content, image, id }) => {
+  const navigate = useNavigate();
+  const navigateToTweet = () => {
+    navigate(`/tweet/${id}`);
+  };
   return (
-    <>
+    <div onClick={navigateToTweet}>
       <p className="tweet-text">{content}</p>
-      <img
-        className="tweet-image"
-        src={imagesrc}
-        alt="Tweet Image"
-        width={350}
-      />
-    </>
+      <img src={image} alt="Tweet Image" height={300} width={400} />
+    </div>
   );
 };
 
